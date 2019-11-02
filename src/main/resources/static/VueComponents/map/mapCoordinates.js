2bf9eb0249b54c9f0bf1f6299ce373d2075aed1f
Bebrevski <stefan.bebrevski89@gmail.com>
2019-11-02 11:47:36 +0200

    Map coordinates ready

--- src/main/resources/static/VueComponents/map/mapCoordinates.js
+++ src/main/resources/static/VueComponents/map/mapCoordinates.js
@@ -1,8 +1,8 @@
 var map;
 
 function initMapCoordinates() {
-    var latitude = 42.6371508;
-    var longitude = 25.7459637;
+    let latitude = 42.6371508;
+    let longitude = 25.7459637;
 
     var myLatLng = {lat: latitude, lng: longitude};
 
@@ -12,39 +12,15 @@ function initMapCoordinates() {
         disableDoubleClickZoom: true
     });
 
-    // Update lat/long value of div when anywhere in the map is clicked
-    google.maps.event.addListener(map, 'click', function (event) {
-        document.getElementById('latclicked').innerHTML = event.latLng.lat();
-        document.getElementById('longclicked').innerHTML = event.latLng.lng();
-    });
-
-    var marker = new google.maps.Marker({
-        position: myLatLng,
-        map: map,
-
-        // setting latitude & longitude as title of the marker
-        // title is shown when you hover over the marker
-        title: latitude + ', ' + longitude
-    });
-
-    // Update lat/long value of div when the marker is clicked
-    marker.addListener('click', function (event) {
-        document.getElementById('latclicked').innerHTML = event.latLng.lat();
-        document.getElementById('longclicked').innerHTML = event.latLng.lng();
-    });
-
     // Create new marker on double click event on the map
-    google.maps.event.addListener(map, 'dblclick', function (event) {
-        var marker = new google.maps.Marker({
-            position: event.latLng,
-            map: map,
-            title: event.latLng.lat() + ', ' + event.latLng.lng()
-        });
+    google.maps.event.addListener(map, 'click', function (event) {
 
-        // Update lat/long value of div when the marker is clicked
-        marker.addListener('click', function () {
-            document.getElementById('latclicked').innerHTML = event.latLng.lat();
-            document.getElementById('longclicked').innerHTML = event.latLng.lng();
+        productionComponent.Production.GPSCoordinates = event.latLng.lat() + ', ' + event.latLng.lng();
+        myLatLng = {lat: event.latLng.lat(), lng: event.latLng.lng()};
+
+        let marker = new google.maps.Marker({
+            position: myLatLng,
+            map: map
         });
     });
 }
