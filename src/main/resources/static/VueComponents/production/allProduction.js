a0d6fa16aa8a29179793a5d48b68c40f9dc5a9f5
Bebrevski <stefan.bebrevski89@gmail.com>
2019-11-09 14:19:57 +0200

    Show all view done

--- src/main/resources/static/VueComponents/production/allProduction.js
+++ src/main/resources/static/VueComponents/production/allProduction.js
@@ -2,6 +2,17 @@ var allProductions = new Vue({
     el: "#allproductions",
     data: {
         productions: [],
+        headers: [
+            { text: 'Име на база', sortable: true, align: 'left', value: 'name', width: "10%" },
+            { text: 'Идентификационен номер', sortable: false, value: 'identifyingNumber', width: "10%" },
+            { text: 'Мейл на базата', sortable: false, value: 'email', width: "17%" },
+            { text: 'Адрес', sortable: false, value: 'fullAddress', width: "15%" },
+            { text: 'GPS координати', sortable: false, value: 'GPSCoordinates', width: "15%" },
+            { text: 'Въведено на', sortable: false, value: 'dateCreated', width: "8%" },
+            { text: 'Описание', sortable: false, value: 'description', width: "20%" },
+            { text: 'Действия', sortable: false, value: null, width: '5%' }
+        ],
+
         loading: true,
     },
     created: function () {
